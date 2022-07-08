1c585cb0458756e079f454cd49f90064fd9f95b6
unknown <allielane24@gmail.com>
2022-07-08 14:22:48 +0200

    9.4

--- api/movies/index.js
+++ api/movies/index.js
@@ -19,4 +19,18 @@ router.get('/:id', (req, res) => {
     }
 });
 
+// Get movie reviews
+router.get('/:id/reviews', (req, res) => {
+    const id = parseInt(req.params.id);
+    // find reviews in list
+    if (movieReviews.id == id) {
+        res.status(200).json(movieReviews);
+    } else {
+        res.status(404).json({
+            message: 'The resource you requested could not be found.',
+            status_code: 404
+        });
+    }
+});
+
 export default router;
