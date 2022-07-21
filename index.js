ce130b7b8897218ca22d37a3c3dffad664eeef6d
unknown <allielane24@gmail.com>
2022-07-21 15:16:47 +0200

    passport

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import userRouter from './api/users';
 import './db';
 import './seedData';
 import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
 
 
 dotenv.config();
@@ -24,18 +24,13 @@ const app = express();
 
 const port = process.env.PORT;
 
-//session middleware
-app.use(session({
-    secret: 'ilikecake',
-    resave: true,
-    saveUninitialized: true,
-}));
+app.use(passport.initialize());
 
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', userRouter);
-app.use('/api/movies', authenticate, moviesRouter);
+app.use('/api/movies', passport.authenticate('jwt', {session:false}), moviesRouter);
 
 app.use(errHandler);
 
