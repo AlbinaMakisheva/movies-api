1f7ad3743154171988935e5a6b0bdc610fd8d8b2
unknown <allielane24@gmail.com>
2022-07-22 11:54:42 +0200

    object referencing

--- api/users/userModel.js
+++ api/users/userModel.js
@@ -2,14 +2,10 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema; //Schema is a constructor function provided by Mongoose for creating schema instances (in this case, UserSchema)
 
-const MovieSchema = new Schema({
-    id: Number,
-    title: String
-});
 
 const UserSchema = new Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    favourites: [MovieSchema]
+    favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movies' }]
 });
 export default mongoose.model('User', UserSchema); //associates the schema instance with a database collection named 'User'
